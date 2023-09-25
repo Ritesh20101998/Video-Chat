a88c5c63717163dc38989c0d197f5913ec79e7d4
Ritesh <kothawaderitesh2010@gmail.com>
2023-09-25 14:13:49 +0530

    complete

--- Frontend/Script/index.js
+++ Frontend/Script/index.js
@@ -2,7 +2,7 @@ import * as state from "./state.js";
 import * as event from "./event.js";
 import * as WebRtc from "./WebRtc.js";
 import * as change from "./change.js"
-const socket = io("http://localhost:8186/", { transports: ["websocket"] });
+const socket = io("https://videochat-uvke.onrender.com/", { transports: ["websocket"] });
 //registering event for socketId
 event.registerSocketEvent(socket);
 
